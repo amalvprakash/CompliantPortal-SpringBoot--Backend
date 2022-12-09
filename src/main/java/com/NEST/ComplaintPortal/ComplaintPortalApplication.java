d12a4004a3877b09e7bd3eb23c212925b06dafc9
Amal <amalprakash2025@gmail.com>
2022-12-09 14:15:57 +0530

    initial commit

---
+++ src/main/java/com/NEST/ComplaintPortal/ComplaintPortalApplication.java
@@ -0,0 +1,13 @@
+package com.NEST.ComplaintPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NEST/ComplaintPortal/ComplaintPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NEST.ComplaintPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
